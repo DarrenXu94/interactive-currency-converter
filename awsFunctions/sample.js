5a38a02e466530021661f6fc06a914f8bd5dc8c4
Darren <darrenxu1994@gmail.com>
2019-02-03 12:18:52 +1100

    aws function tetsing

--- awsFunctions/sample.js
+++ awsFunctions/sample.js
@@ -1,6 +1,6 @@
 exports.handler = function(event, context, callback) {
     callback(null, {
     statusCode: 200,
-    body: "Hello, World"
+    body: `Hello, World, you are running on ${process.env.NODE_ENV} with ${process.env.API_KEY}`
     });
 }
