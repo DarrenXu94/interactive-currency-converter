88db7d771c312898171dd8794ee0cc915dc0e795
Darren <darrenxu1994@gmail.com>
2019-02-16 10:23:56 +1100

    Local testing working

--- api/Currency.js
+++ api/Currency.js
@@ -7,21 +7,48 @@ var request = require("request");
 // }
 
 module.exports = async (from,to, key) => {
+
+  if (process.env.NODE_ENV === 'production') {
+
+    console.log('prod')
+
     return new Promise((res,rej)=>{  
-        var options = {
-          method: 'GET',
-          url: `https://api.currencystack.io/currency?base=${from}&target=${to}`,
-          qs: { apikey: key },
-          headers:
-          {
-            'postman-token': '0010f3d4-76e5-d044-31f3-42ecc4260371',
-            'cache-control': 'no-cache'
-          }
-        };
-        request(options, function (error, response, body) {      
-          console.log(body);
-          res(body)
-          
-        })
+      var options = {
+        method: 'GET',
+        url: `https://api.currencystack.io/currency?base=${from}&target=${to}`,
+        qs: { apikey: key },
+        headers:
+        {
+          'postman-token': '0010f3d4-76e5-d044-31f3-42ecc4260371',
+          'cache-control': 'no-cache'
+        }
+      };
+      request(options, function (error, response, body) {      
+        console.log(body);
+        res(body)
+        
       })
+    })
+  } else {
+    let key = require('./Private')
+    console.log(key)
+    return new Promise((res,rej)=>{  
+      var options = {
+        method: 'GET',
+        url: `https://api.currencystack.io/currency?base=${from}&target=${to}`,
+        qs: { apikey: key },
+        headers:
+        {
+          'postman-token': '0010f3d4-76e5-d044-31f3-42ecc4260371',
+          'cache-control': 'no-cache'
+        }
+      };
+      request(options, function (error, response, body) {      
+        console.log(body);
+        res(body)
+        
+      })
+    })
+
+  }
 }
